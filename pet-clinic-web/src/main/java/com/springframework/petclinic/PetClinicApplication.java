d9afa57c300fa20d19ad25ae512cd5857b793d33
Nikos Bartsotas <nickbar86@hotmail.com>
2020-05-08 14:19:13 +0300

    Added Modules and Release

--- pet-clinic-web/src/main/java/com/springframework/petclinic/PetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/PetClinicApplication.java
@@ -2,12 +2,13 @@ package com.springframework.petclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class PetClinicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PetClinicApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(PetClinicApplication.class, args);
     }
 
 }
